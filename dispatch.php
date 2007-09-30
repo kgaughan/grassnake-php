7ac4ccf86774b8efc5b7d0a5643bccb487999876
keith <>
2007-09-30 23:35:29 +0000

    [project @ 130] Altered for new bootstrapping world order.

--- dispatch.php
+++ dispatch.php
@@ -1,18 +1,3 @@
 <?php
-define('APP_ROOT', dirname(__FILE__));
-define('APP_TEMPLATE_ROOT', APP_ROOT . '/templates');
-
-require(APP_ROOT . '/lib/afk/afk.php');
-
-AFK::add_class_path(APP_ROOT . '/lib/classes');
-AFK::add_class_path(APP_ROOT . '/classes');
-AFK::add_class_path(APP_ROOT . '/handlers');
-
-AFK_Registry::set_instance(new AFK_Registry());
-
-include(APP_ROOT . '/config.php');
-if (file_exists(APP_ROOT . '/lib/lib.php')) {
-	include(APP_ROOT . '/lib/lib.php');
-}
-
-AFK::process_request(routes(), init());
+require(dirname(__FILE__) . '/bootstrap.php');
+AFK::process_request(AFK_Registry::routes(), $filters);
