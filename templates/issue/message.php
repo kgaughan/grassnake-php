cc848cfc8c90a934c685281e12e311a4fd999b8a
Keith Gaughan <keith@talideon.com>
2007-10-30 00:33:48 +0000

    Minor UI improvements to the Issue screen to make the changes listing clearer.

--- templates/issue/message.php
+++ templates/issue/message.php
@@ -1,7 +1,7 @@
 <?php if ($current_row == 1) { ?>
 	<h2>Changes</h2>
 <?php } ?>
-<div class="message<?php if ($current_row == 0) echo ' first' ?>">
+<div class="message<?php if ($current_row == 0) { ?> first<?php } elseif ($current_row % 2 == 0) { ?> even<?php } ?>">
 	<div class="metadata">
 	<strong><?php ee(Users::get($user_id)->get_username()) ?></strong><br>
 	<?php if ($current_row > 0) { ?>
